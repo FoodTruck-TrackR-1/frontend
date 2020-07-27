7c3addb8685ce73222b5d562bd66fc59082d2586
Daniel Antonio <danieltomantonio@gamil.com>
2020-07-27 10:57:17 -0700

    Init signup commit

--- food-truck/src/components/Signup.js
+++ food-truck/src/components/Signup.js
@@ -4,6 +4,30 @@ const Signup = () => {
   return (
     <div className='container'>
       <h2>Sign Up</h2>
+      <div className='form-container'>
+      <form>
+        <div className='form-header'>
+          <p>Welcome to<br/>Foodtruck TrackR</p>
+          <h2>Sign Up</h2>
+        </div>
+        <div className='form-inputs'>
+          <label>Email: </label>
+          <input type='email' placeHolder='Email@example.com' />
+          <label>Password: </label>
+          <input type='password' placeHolder='Password of 6 chars min' />
+          <label>Confirm Password: </label>
+          <input type='password' placeHolder='Confirm Password' />
+          <label>Sign Up As: </label>
+          <select name='account-type'>
+            <option value=''>- Select account type -</option>
+            <option value='user'>User</option>
+          </select>
+          <div className='form-submit-container'>
+            <button type='submit' value='Submit'>Sign Up</button>
+          </div>
+        </div>
+      </form>
+    </div>
     </div>
   );
 };
