f4e6445c3cba01aa1adec24d2cac58fd0176ca4d
arianahabas <arianahabas@gmail.com>
2020-07-27 13:44:16 -0500

    basic form and basic styles

--- food-truck/src/components/Login.js
+++ food-truck/src/components/Login.js
@@ -1,34 +1,59 @@
 import React from "react";
+import styled from 'styled-components'
+
+const FormContainer = styled.div `
+  display: flex;
+  flex-direction: column;
+  
+
+  .header {
+    text-align: center;
+  }
+  .inputs {
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    margin: 0 auto;
+    
+  }
+`;
 
 const Login = () => {
   return (
-    <div className='container'>
-      <h2>Login</h2>
+    <FormContainer className='container'>
       <form>
-        <label>
-         username:
-         <input 
-          name='username'
-          type='textbox'
-          />
-        </label>
-        <label>
-          passord:
-          <input
-          name='username'
-          type='password'
-          />
-        </label>
-        <label>
-          Login as:
-          <select name='user'>
-            <option value='operator'>Operator</option>
-            <option value='diner'>Diner</option>
-          </select>
-        </label>
-        <button type='submit'>Login</button>
+        <div className='header'>
+          <p>Welcome to<br/>Foodtruck TrackR</p>
+          <h2>Login</h2>
+        </div>
+        <div className='inputs'>
+          <label>
+          username:
+          <input 
+            name='username'
+            type='textbox'
+            />
+          </label>
+          <label>
+            passord:
+            <input
+            name='username'
+            type='password'
+            placeholder='name@email.com'
+            />
+          </label>
+          <label>
+            Login as:
+            <select name='user'>
+              <option value='operator'>-Select accout type-</option>
+              <option value='operator'>Truck Owner</option>
+              <option value='diner'>User</option>
+            </select>
+          </label>
+          <button type='submit'>Login</button>
+          </div>
       </form>
-    </div>
+    </FormContainer>
   );
 };
 
