59ba05128ac5eae5ddc65137b954eeb6eac5f639
arianahabas <arianahabas@gmail.com>
2020-07-27 13:10:33 -0500

    initial login commit

--- food-truck/src/components/Login.js
+++ food-truck/src/components/Login.js
@@ -4,6 +4,30 @@ const Login = () => {
   return (
     <div className='container'>
       <h2>Login</h2>
+      <form>
+        <label>
+         username:
+         <input 
+          name='username'
+          type='textbox'
+          />
+        </label>
+        <label>
+          passord:
+          <input
+          name='username'
+          type='password'
+          />
+        </label>
+        <label>
+          Login as:
+          <select name='user'>
+            <option value='operator'>Operator</option>
+            <option value='diner'>Diner</option>
+          </select>
+        </label>
+        <button type='submit'>Login</button>
+      </form>
     </div>
   );
 };
