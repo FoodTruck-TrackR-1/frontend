0b653698cbf26e8835289110a8d4a7d6b1357c05
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-29 16:45:11 -0500

    🛠 SET UP: UserContext.Provider + ReactQueryDevtools (App.js)

--- food-truck/src/App.js
+++ food-truck/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { ReactQueryDevtools } from "react-query-devtools";
+import UserContext from "./contexts/UserContext";
 import "./App.css";
 import PrivateRoute from "./components/PrivateRoute";
 import Header from "./components/Header";
@@ -8,18 +9,31 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Food from "./components/Food";
 
+const initUser = {
+  id: undefined,
+  is_operator: false,
+  name: "",
+  username: "",
+};
+
 function App() {
+  const [user, setUser] = React.useState(initUser);
+  console.log("App -> User:", user);
+
   return (
     <Router>
-      <div className='App'>
-        <Header />
-        <Switch>
-          <Route exact path='/' />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
-          <PrivateRoute path='/food' component={Food} />
-        </Switch>
-      </div>
+      <UserContext.Provider value={{ user, setUser }}>
+        <div className='App'>
+          <Header />
+          <Switch>
+            <Route exact path='/' />
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={Signup} />
+            <PrivateRoute path='/food' component={Food} />
+          </Switch>
+          <ReactQueryDevtools />
+        </div>
+      </UserContext.Provider>
     </Router>
   );
 }
