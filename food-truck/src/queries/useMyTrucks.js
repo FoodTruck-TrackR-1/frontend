758e018fdce1cbf8c00915f1ecb82eb7b2063f85
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-30 12:05:02 -0500

    🙈 DUMB: removed BASE_URL from axiosWithAuth calls

--- food-truck/src/queries/useMyTrucks.js
+++ food-truck/src/queries/useMyTrucks.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-const BASE_URL = "https://foodtruck-bw.herokuapp.com/api/";
 
 export default function useMyTrucks({user}) {
   return useQuery(`${user.id}/trucks`, () => {
     return axiosWithAuth()
-      .get(`${BASE_URL}operators/${user.id}/trucks`)
+      .get(`operators/${user.id}/trucks`)
       .then((res) => res.data.data);
   });
 }
