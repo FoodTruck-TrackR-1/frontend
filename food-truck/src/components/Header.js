26810189d9d23f73d4e2d087c709dbf7bee13c23
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-31 09:10:53 -0500

    ⚔️ SET UP: loggout button in header

--- food-truck/src/components/Header.js
+++ food-truck/src/components/Header.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+  const logoutToken = () => {
+    localStorage.removeItem("token");
+  };
+
   return (
     <div className='nav-bar'>
       <h1>Food Truck Tracker</h1>
@@ -18,6 +22,11 @@ const Header = () => {
         </li>
         <li>
           <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/' onClick={logoutToken}>
+            Log Out
+          </Link>
         </li>
       </ul>
     </div>
